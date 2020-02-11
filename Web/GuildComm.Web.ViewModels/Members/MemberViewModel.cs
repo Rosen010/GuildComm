559a259ab9fd5838ae5a3de7ae99c423201d8e82
Rosen010 <rosen010@mail.bg>
2020-02-11 10:28:30 +0200

    Add Member view model.

---
+++ Web/GuildComm.Web.ViewModels/Members/MemberViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GuildComm.Web.ViewModels.Members
+{
+    using GuildComm.Web.ViewModels.Characters;
+    using System;
+
+    public class MemberViewModel
+    {
+        public CharacterViewModel Character { get; set; }
+
+        public string Rank { get; set; }
+
+        public DateTime MemberSince { get; set; }
+    }
+}
