7eede881d02e7f2c9e9fda472b14d1ae3f76449d
Rosen <Rosen010@mail.bg>
2021-10-07 23:30:59 +0300

    Work on Battle.net authentication.

---
+++ Services/GuildComm.Services/Clients/BNetApiClient.cs
@@ -0,0 +1,48 @@
+﻿using GuildComm.Services.Models;
+using GuildComm.Services.Utilities.Constants;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildComm.Services.Clients
+{
+    public class BNetApiClient
+    {
+        private readonly string _clientId;
+
+        private readonly string _clientSecret;
+
+        private BNetBearerToken _accessToken;
+
+        private static HttpClient _authClient;
+
+        public BNetApiClient(string clientId, string clientSecret)
+        {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }
+
+        public async Task Authenticate()
+        {
+            var url = "https://us.battle.net/oauth/token";
+
+            using (var httpRequest = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(_clientId + ":" + _clientSecret));
+
+                httpRequest.Headers.Add(RequestHeaders.Authorization, $"Basic {credentials}");
+                httpRequest.Headers.Add(RequestHeaders.GrantType, "client_credentials");
+
+                using (var httpResponse = await _authClient.SendAsync(httpRequest))
+                {
+                    var json = await httpResponse.Content.ReadAsStringAsync();
+                    var response = JsonConvert.DeserializeObject<BNetBearerToken>(json);
+
+                    _accessToken = response;
+                }
+            }
+        }
+    }
+}
---
+++ Services/GuildComm.Services/Models/BNetBearerToken.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace GuildComm.Services.Models
+{
+    internal class BNetBearerToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int Expiration { get; set; }
+    }
+}
