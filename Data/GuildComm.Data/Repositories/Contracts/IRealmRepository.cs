d5ae5aca1e0bbccf8ac96452363c2fe83e399ce8
Rosen <Rosen010@mail.bg>
2021-10-17 16:15:50 +0300

    Add realm repository.

---
+++ Data/GuildComm.Data/Repositories/Contracts/IRealmRepository.cs
@@ -0,0 +1,12 @@
+﻿using GuildComm.Data.Models;
+using System.Collections.Generic;
+
+namespace GuildComm.Data.Repositories.Contracts
+{
+    public interface IRealmRepository
+    {
+        IEnumerable<Realm> GetAll();
+
+        void InsertAll(IEnumerable<Realm> realms);
+    }
+}
