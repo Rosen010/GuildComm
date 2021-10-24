f5464001a8afb84a373ebc97b4ac2a2d99edce6f
Rosen <Rosen010@mail.bg>
2021-10-24 21:12:58 +0300

    Build guild response model.

---
+++ Services/GuildComm.Services/Models/ResponseModels/ResponseComponents/FactionComponent.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace GuildComm.Services.Models.ResponseModels.ResponseComponents
+{
+    public class FactionComponent
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Services/GuildComm.Services/Models/ResponseModels/ResponseComponents/RosterComponent.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace GuildComm.Services.Models.ResponseModels.ResponseComponents
+{
+    public class RosterComponent
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
+    }
+}
