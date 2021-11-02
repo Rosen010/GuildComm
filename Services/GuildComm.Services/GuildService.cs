e6d09ded9be6669b258025d16a0971b138eab337
Rosen <Rosen010@mail.bg>
2021-11-02 15:56:24 +0200

    Projects renaming

--- Services/GuildComm.Services/GuildService.cs
+++ Services/GuildComm.Services/GuildService.cs
@@ -1,9 +1,15 @@
 ﻿using GuildComm.Services.Contracts;
+using GuildComm.Services.Contracts.Clients;
 
 namespace GuildComm.Services
 {
     public class GuildService : IGuildService
     {
-        
+        private readonly IBNetGuildClient _guildClient;
+
+        public GuildService(IBNetGuildClient guildClient)
+        {
+            _guildClient = guildClient;
+        }
     }
 }
---
+++ Web/GuildComm.Web/Controllers/SearchController.cs
@@ -0,0 +1,15 @@
+﻿using GuildComm.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuildComm.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IGuildService _guildService;
+
+        public SearchController(IGuildService guildService)
+        {
+            _guildService = guildService;
+        }
+    }
+}
