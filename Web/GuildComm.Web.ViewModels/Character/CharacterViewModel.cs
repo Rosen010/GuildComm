62a3243cdddcfb7475a4ee63daf4c6b69f684876
Rosen <Rosen010@mail.bg>
2022-02-11 22:35:24 +0200

    Create character view model.

---
+++ Web/GuildComm.Web.ViewModels/Character/CharacterViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace GuildComm.Web.Models.Character
+{
+    public class CharacterViewModel
+    {
+        public string Name { get; set; }
+
+        public string Faction { get; set; }
+
+        public string Race { get; set; }
+
+        public string Class { get; set; }
+
+        public string Spec { get; set; }
+
+        public string Realm { get; set; }
+
+        public string Guild { get; set; }
+
+        public int AvgItemLevel { get; set; }
+
+        public int EquippedItemLevel { get; set; }
+    }
+}
