5edea35d9123308425581f5d9ff61bab84e6edfa
Rosen <Rosen010@mail.bg>
2022-03-13 12:47:11 +0200

    Add realms repository

---
+++ Data/GuildComm.Data/Repositories/Interfaces/IRealmsRepository.cs
@@ -0,0 +1,10 @@
+﻿using GuildComm.Data.Enums;
+using GuildComm.Data.Models;
+
+namespace GuildComm.Data.Repositories.Interfaces
+{
+    public interface IRealmsRepository
+    {
+        Task<IEnumerable<Realm>> GetRealmsByRegion(Region region);
+    }
+}
---
+++ Data/GuildComm.Data/Repositories/RealmsRepository.cs
@@ -0,0 +1,23 @@
+﻿using GuildComm.Data.Enums;
+using GuildComm.Data.Models;
+using GuildComm.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuildComm.Data.Repositories
+{
+    public class RealmsRepository : IRealmsRepository
+    {
+        private readonly GuildCommDbContext _dbContext;
+
+        public RealmsRepository(GuildCommDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Realm>> GetRealmsByRegion(Region region)
+        {
+            var realms = await _dbContext.Realms.Where(r => r.Region.Equals(region)).ToListAsync();
+            return realms;
+        }
+    }
+}
