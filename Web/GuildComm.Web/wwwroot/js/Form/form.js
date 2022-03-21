ffa82d96c4356202a3713dc9b97e7ace4747e35f
Rosen <Rosen010@mail.bg>
2022-03-21 13:35:55 +0200

    Fix js event binding issues.

---
+++ Web/GuildComm.Web/wwwroot/js/Form/form.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    function updateRealms(region) {
+        let data = { region: region };
+
+        return $.ajax({
+            url: '/RealmsApi/UpdateRealms',
+            type: 'GET',
+            data: data,
+            contentType: 'application/json'
+        }).done(function (response) {
+            if (response) {
+                $('#realmSelect').html('');
+                let options = '';
+
+                for (var i = 0; i < response.length; i++) {
+                    options += '<option value="' + response[i] + '">' + response[i] + '</option>';
+                }
+
+                $('#realmSelect').append(options);
+            }
+        }).fail(function (error) {
+            console.log(error.statusText);
+        });
+    }
+
+    function bindChangeEvent(formId) {
+            $(formId).on('change', function () {
+                let region = $(formId + " option:selected").text()
+                updateRealms(region);
+            });
+    }
+
+    function setup() {
+        bindChangeEvent("#guildRegionSelect");
+
+        $("#characterForm").click(function () {
+            $("#formContainer").load("/CharactersApi/CharacterForm", function () {
+                bindChangeEvent("#characterRegionSelect");
+            });
+        });
+    }
+
+    setup();
+});
