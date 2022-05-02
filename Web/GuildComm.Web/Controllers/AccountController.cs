3408e674d59700bf2def4063a5fa04ddf915597c
Rosen <Rosen010@mail.bg>
2022-05-02 10:29:12 +0300

    Add registration page mvc.

---
+++ Web/GuildComm.Web/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using GuildComm.Web.Models.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuildComm.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(UserRegistrationInputModel userModel)
+        {
+            return this.View();
+        }
+    }
+}
